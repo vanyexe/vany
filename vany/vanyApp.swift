9fe4d8dfdc1fa78ceceda977311cfbb77d307a64
Vansh Shailendra Patil <patilvansh2417@gmail.com>
2025-12-04 15:38:09 +0530

    Update vanyApp.swift

--- vany/vanyApp.swift
+++ vany/vanyApp.swift
@@ -15,3 +15,37 @@ struct vanyApp: App {
         }
     }
 }
+import SwiftUI
+import SwiftData
+struct piyushpratice3App : View {
+//used to perform crud operations
+@Environment(\.modelContext) private
+var modelContext
+@Query private var Lists:[listt]
+var body: some View{
+NavigationStackd
+List{
+ForEach(Lists)
+list in
+Text(list.title)
+.swipeActionsd
+1
+Button("Delete", role:.destructive){
+modelContext.delete(list)
+33
+.navigationTitle("My ToDo")
+.toolbar{
+ToolbarItem(placement:
+33
+.topBarTrailing)
+Buttond
+} label:{
+Image(systemName:"plus.circle")
+overlay{
+if Lists.isEmpty{
+4449%9m645658
+ContentUnavailableView("My lists are unavilabel"，systemxmages“pus，e主Fele*，desbptient fext(*NG 王海ste
+yet. Add one to get started"))
+#Preview("Main List"){
+piyushpratice3App()
+modelContainer(for: listt.self, inMemoryi true)
