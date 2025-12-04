1c7af8281328cc2483409331b7d5b04ff20197d8
Vansh Patil <patilvansh2417@gmail.com>
2025-12-04 11:54:39 +0530

    Initial Commit

---
+++ vany/vanyApp.swift
@@ -0,0 +1,17 @@
+//
+//  vanyApp.swift
+//  vany
+//
+//  Created by Teacher on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct vanyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
